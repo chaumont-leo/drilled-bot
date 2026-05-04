13061c0774348cd130b7733589da801028cd10f7
Léo <leo.chaumont15@gmail.com>
2026-05-04 21:30:23 +0200

    feat(welcome): add config options for embed message

--- src/modules/welcome/listeners/WelcomeListener.js
+++ src/modules/welcome/listeners/WelcomeListener.js
@@ -19,19 +19,17 @@ class WelcomeListener extends BaseListener {
 					? await member.guild.channels.fetch(configManager.getConfigValue('welcome.channel'))
 					: member.guild.systemChannel;
 
-				const ticketChannelId = configManager.getConfigValue('ticket.channel');
-
 				if (channel) {
-					const embed = new EmbedBuilder()
-						.setTitle('**Bienvenue chez la DRILLED**')
-						.setColor('#ffffff') // Vous pouvez définir des couleurs avec des chaînes de caractères hexadécimales, des nombres ou des strings prédéfinis
-						.setDescription(`Bienvenue ${member} ! #${member.guild.memberCount} \n Si tu souhaites être recruté ouvre un ticket \n <#${ticketChannelId}>`)
-						.setThumbnail('attachment://logo.png')
-						.setFooter({
-							text: 'DRILLED',
-							iconURL: 'attachment://logoWithoutBg.png'
-						})
-						.setTimestamp();
+					const embed = new EmbedBuilder();
+
+					this.checkAndSetIfActive('title', (value) => embed.setTitle(value));
+					this.checkAndSetIfActive('color', (value) => embed.setColor(value));
+					this.checkAndSetIfActive('description', (value) => embed.setDescription(this.getFormattedDescription(value, member)));
+
+					this.setupThumbnail(embed);
+
+					this.setupFooter(embed);
+
 					await channel.send({ embeds: [embed], files: attachments });
 				}
 
@@ -42,6 +40,44 @@ class WelcomeListener extends BaseListener {
 				console.error(e);
 			}
 		})
+	}
+
+	checkAndSetIfActive(property, cb) {
+		const value = configManager.getConfigValue(`welcome.embedMessage.${property}`);
+		if(value) cb(value);
+	}
+
+	getFormattedDescription(rawDescription, member) {
+		return rawDescription
+			.replaceAll('%member%', member)
+			.replaceAll('%count%', member.guild.memberCount)
+			.replaceAll('%ticketChannel%', `<#${configManager.getConfigValue('ticket.channel')}>`)
+	}
+
+	setupThumbnail(embed) {
+		if(configManager.getConfigValue('welcome.embedMessage.thumbnail') === 'LOGO') {
+			const path = configManager.getConfigValue('welcome.embedMessage.logoPath')
+			if(path) embed.setThumbnail(`attachment://${path}`);
+		}
+	}
+
+	setupFooter(embed) {
+		if(configManager.getConfigValue('welcome.embedMessage.footer.active')) {
+			const footer = {};
+
+			const icon = configManager.getConfigValue('welcome.embedMessage.footer.icon');
+			const iconPath = configManager.getConfigValue('welcome.embedMessage.footer.icon');
+			if(icon && iconPath) footer.iconURL = `attachment://${iconPath}`;
+
+			const text = configManager.getConfigValue('welcome.embedMessage.footer.text');
+			if(text) footer.text = text;
+
+			embed.setFooter(footer);
+
+			if(configManager.getConfigValue('welcome.embedMessage.footer.timestamp')) {
+				embed.setTimestamp();
+			}
+		}
 	}
 }
 
